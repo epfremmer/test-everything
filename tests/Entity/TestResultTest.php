1a9606d0ef271dd65b9318a9b65bd96a5dac2a0a
epfremme <epfremme@nerdery.com>
2015-12-18 15:39:44 -0600

    Added test result test

--- tests/Entity/TestResultTest.php
+++ tests/Entity/TestResultTest.php
@@ -1,15 +1,42 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
- * User: epfremme
- * Date: 12/18/15
- * Time: 1:43 AM
+ * File TestResultTest.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
  */
-
 namespace Epfremme\Everything\Tests\Entity;
 
+use Epfremme\Everything\Entity\TestResult;
 
+/**
+ * Class TestResultTest
+ *
+ * @package Epfremme\Everything\Tests\Entity
+ */
 class TestResultTest extends \PHPUnit_Framework_TestCase
 {
+    const TEST_RESULT_TEXT = "\nTest header\n\n...\n\nOK (3 tests, 3 assertions)";
 
+    public function testConstruct()
+    {
+        $testResult = new TestResult(sha1('abc'), self::TEST_RESULT_TEXT);
+
+        $this->assertInstanceOf(TestResult::class, $testResult);
+        $this->assertAttributeEquals(sha1('abc'), 'hash', $testResult);
+        $this->assertAttributeEquals(self::TEST_RESULT_TEXT, 'result', $testResult);
+    }
+
+    public function testGetHash()
+    {
+        $testResult = new TestResult(sha1('abc'), self::TEST_RESULT_TEXT);
+
+        $this->assertEquals(sha1('abc'), $testResult->getHash());
+    }
+
+    public function testGetResult()
+    {
+        $testResult = new TestResult(sha1('abc'), self::TEST_RESULT_TEXT);
+
+        $this->assertEquals(self::TEST_RESULT_TEXT, $testResult->getResult());
+    }
 }
