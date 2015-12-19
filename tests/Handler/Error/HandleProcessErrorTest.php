55f5e57e6924c5ea7c7fe5eeea51aa9e145c59ba
epfremme <epfremme@nerdery.com>
2015-12-19 17:11:38 -0600

    Added process error handler tests

--- tests/Handler/Error/HandleProcessErrorTest.php
+++ tests/Handler/Error/HandleProcessErrorTest.php
@@ -1,15 +1,60 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
- * User: epfremme
- * Date: 12/18/15
- * Time: 1:45 AM
+ * File HandleProcessErrorTest.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
  */
-
 namespace Epfremme\Everything\Tests\Handler\Error;
 
+use Epfremme\Collection\Collection;
+use Epfremme\Everything\Entity\TestResult;
+use Epfremme\Everything\Handler\Error\HandleProcessError;
+use Symfony\Component\Process\Process;
 
+/**
+ * Class HandleProcessErrorTest
+ *
+ * @package Epfremme\Everything\Tests\Handler\Error
+ */
 class HandleProcessErrorTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Collection
+     */
+    private $results;
 
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->results = new Collection();
+    }
+
+    public function testConstruct()
+    {
+        $handler = new HandleProcessError($this->results);
+
+        $this->assertAttributeSame($this->results, 'results', $handler);
+    }
+
+    public function testInvoke()
+    {
+        $handler = new HandleProcessError($this->results);
+        $process = \Mockery::mock(Process::class);
+
+        $process->shouldReceive('getWorkingDirectory->getFilename')->once()->andReturn(sha1(''));
+        $process->shouldIgnoreMissing();
+
+        /** @var TestResult $result */
+        $result = $handler($process);
+
+        $this->assertInstanceOf(TestResult::class, $result);
+        $this->assertEquals($result->getResult(), 'ERROR');
+        $this->assertEquals($result->getHash(), sha1(''));
+        $this->assertCount(1, $this->results);
+        $this->assertContainsOnlyInstancesOf(TestResult::class, $this->results);
+    }
 }
