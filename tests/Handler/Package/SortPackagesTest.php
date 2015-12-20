05ebfe4568929f895e5be46e1c1374ca0b96854f
epfremme <epfremme@nerdery.com>
2015-12-19 18:28:49 -0600

    Added sort packages handler test

--- tests/Handler/Package/SortPackagesTest.php
+++ tests/Handler/Package/SortPackagesTest.php
@@ -6,6 +6,13 @@
  */
 namespace Epfremme\Everything\Tests\Handler\Package;
 
+use Epfremme\Collection\Collection;
+use Epfremme\Everything\Entity\Package;
+use Epfremme\Everything\Handler\Package\SortPackages;
+use Epfremme\Everything\Tests\Entity\PackageTest;
+use Epfremme\Everything\Tests\Traits\SerializerTrait;
+
+
 /**
  * Class SortPackagesTest
  *
@@ -13,13 +20,43 @@ namespace Epfremme\Everything\Tests\Handler\Package;
  */
 class SortPackagesTest extends \PHPUnit_Framework_TestCase
 {
+    use SerializerTrait;
+
+    /**
+     * @var Collection
+     */
+    private $packages;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->packages = new Collection([
+            'b/b' => $this->getSerializer()->deserialize(str_replace('test/test', 'b/b', PackageTest::TEST_PACKAGE_JSON), Package::class, 'json'),
+            'a/a' => $this->getSerializer()->deserialize(str_replace('test/test', 'a/a', PackageTest::TEST_PACKAGE_JSON), Package::class, 'json'),
+            'c/c' => $this->getSerializer()->deserialize(str_replace('test/test', 'c/c', PackageTest::TEST_PACKAGE_JSON), Package::class, 'json'),
+        ]);
+    }
+
     public function testConstruct()
     {
+        $handler = new SortPackages($this->packages);
 
+        $this->assertAttributeSame($this->packages, 'packages', $handler);
     }
 
     public function testInvoke()
     {
+        $handler = new SortPackages($this->packages);
+        $packages = $handler();
 
+        $this->assertSame($this->packages, $packages);
+        $this->assertEquals('a/a', $this->packages->current()->getName());
+        $this->assertEquals('b/b', $this->packages->next()->getName());
+        $this->assertEquals('c/c', $this->packages->next()->getName());
+        $this->assertEquals(['a/a', 'b/b', 'c/c'], $this->packages->getKeys());
     }
 }
