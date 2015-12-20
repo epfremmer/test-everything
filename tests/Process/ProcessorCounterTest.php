497789ee2c94e2be947036d48ac4c84ccfe074bf
epfremme <epfremme@nerdery.com>
2015-12-20 01:22:01 -0600

    Added processor counter test

--- tests/Process/ProcessorCounterTest.php
+++ tests/Process/ProcessorCounterTest.php
@@ -6,6 +6,8 @@
  */
 namespace Epfremme\Everything\Tests\Process;
 
+use Epfremme\Everything\Process\ProcessorCounter;
+
 /**
  * Class ProcessorCounterTest
  *
@@ -13,43 +15,21 @@ namespace Epfremme\Everything\Tests\Process;
  */
 class ProcessorCounterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testConstruct()
+    public function testGetCpuCount()
     {
+        $processorCounter = new ProcessorCounter();
+        $processorCount = $processorCounter->getCpuCount();
 
+        $this->assertInternalType('integer', $processorCount);
+        $this->assertGreaterThan(0, $processorCount);
     }
 
     public function testToString()
     {
+        $processorCounter = new ProcessorCounter();
+        $processorCount = (string) $processorCounter;
 
-    }
-
-    private function testHasProcInfo()
-    {
-
-    }
-
-    private function testIsWindows()
-    {
-
-    }
-
-    public function testGetCpuCount()
-    {
-
-    }
-
-    private function testGetSysctlCount()
-    {
-
-    }
-
-    private function testGetProcCount()
-    {
-
-    }
-
-    private function testGetWinCount()
-    {
-
+        $this->assertInternalType('string', $processorCount);
+        $this->assertGreaterThan(0, $processorCount);
     }
 }
